e8abc47b93938cc74af364054481a8f42190836e
DNYANUU <harshadwakade1234@gmail.com>
2026-08-02 01:12:24 +0530

    Update homevid.tsx

--- components/home/header/homecomp/homevid.tsx
+++ components/home/header/homecomp/homevid.tsx
@@ -1,17 +1,22 @@
-
 export default function HomeHeaderVid() {
-    return (
-        <>
-            <div className="inset-0 absolute -z-10">
-                <video autoPlay loop muted playsInline disablePictureInPicture controlsList="nodownload"
-                    className="absolute w-[100%] h-screen block pointer-events-none object-cover">
-                    <source src={'https://res.cloudinary.com/dnqbr6fyr/video/upload/v1785433610/bgcompressed_qlzqgi.mp4'} type="video/mp4" />
-                </video>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <video 
+          autoPlay 
+          loop 
+          muted 
+          playsInline 
+          disablePictureInPicture 
+          controlsList="nodownload"
+          className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
+        >
+          <source 
+            src="https://res.cloudinary.com/dnqbr6fyr/video/upload/v1785433610/bgcompressed_qlzqgi.mp4" 
+            type="video/mp4" 
+          />
+        </video>
+      </div>
+    </>
+  );
 }
-
-
-//bg-video absolute w-[100%] h-screen block pointer-events-none object-cover
-// absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover
