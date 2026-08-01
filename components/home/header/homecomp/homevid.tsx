648df8e3fd9c88e38b6e4fa37fc5da891294b25c
DNYANUU <harshadwakade1234@gmail.com>
2026-08-02 01:19:26 +0530

    Update homevid.tsx

--- components/home/header/homecomp/homevid.tsx
+++ components/home/header/homecomp/homevid.tsx
@@ -1,7 +1,7 @@
 export default function HomeHeaderVid() {
   return (
     <>
-      <div className="absolute inset-0 -z-10 overflow-hidden">
+      <div className="absolute inset-0 -z-10 overflow-hidden bg-black">
         <video 
           autoPlay 
           loop 
@@ -9,7 +9,7 @@ export default function HomeHeaderVid() {
           playsInline 
           disablePictureInPicture 
           controlsList="nodownload"
-          className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
+          className="absolute top-1/2 left-1/2 min-w-[101vw] min-h-[101vh] w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none scale-105"
         >
           <source 
             src="https://res.cloudinary.com/dnqbr6fyr/video/upload/v1785433610/bgcompressed_qlzqgi.mp4" 
