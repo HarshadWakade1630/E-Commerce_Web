9de379b93b977aa989b9521b786629620a4e49a3
Harshad Wakade <harshadwakade1234@gmail.com>
2026-08-16 19:40:41 +0530

    L

--- components/food/header/navbar.tsx
+++ components/food/header/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function FoodNavbar() {
   return (
     <>
-      <header className="bg-[#ce1f2c] h-[120px] sm:h-[160px] w-full rounded-b-[15px]">
+      <header className="bg-[#ce1f2c] h-[150px] sm:h-[160px] w-full rounded-b-[15px]">
         <div className="header-container h-full w-full flex flex-col sm:flex-row justify-between items-start sm:items-center px-[3%] pt-[3%] py-[1%]">
 
           {/* Logo */}
@@ -15,16 +15,15 @@ export default function FoodNavbar() {
           </div>
 
           {/* Links */}
-          <div className="h-full flex items-end self-end sm:self-auto pb-[0.5] sm:pb-0">
+          <div className="h-full flex items-end self-end sm:self-auto pb-2 sm:pb-0">
             <nav className="flex text-white">
-              {/* Added subtle pb-1.5 for mobile mode to push links towards the bottom */}
-              <Link className="mr-4 sm:mr-[30px] pb-[0.5] sm:pb-[5px] text-xs sm:text-sm" href="/food/restaurants">
+              <Link className="mr-4 sm:mr-[30px] pb-1 sm:pb-[5px] text-xs sm:text-sm" href="/food/restaurants">
                 Restaurants
               </Link>
-              <Link className="mr-4 sm:mr-[30px] pb-[0.5] sm:pb-[5px] text-xs sm:text-sm" href="/food/myorders">
+              <Link className="mr-4 sm:mr-[30px] pb-1 sm:pb-[5px] text-xs sm:text-sm" href="/food/myorders">
                 My Orders
               </Link>
-              <Link className="mr-0 sm:mr-[30px] pb-[0.5] sm:pb-[5px] text-xs sm:text-sm" href="/account">
+              <Link className="mr-0 sm:mr-[30px] pb-1 sm:pb-[5px] text-xs sm:text-sm" href="/account">
                 Account
               </Link>
             </nav>
