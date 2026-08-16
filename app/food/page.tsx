19f381a5b16953070897f5cee701a55e716db86b
Harshad Wakade <harshadwakade1234@gmail.com>
2026-08-16 20:14:08 +0530

    L

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -2,7 +2,7 @@ import FoodFooter from "@/components/food/footer/footer";
 import FoodNavbar from "@/components/food/header/navbar";
 import FoodMain from "@/components/food/main/main";
 import StickyNavbarController from "@/components/food/header/stickynavbarcontroller";
-import BackToTop from "@/components/common/backtotop";
+import BackToTop from "@/components/common/BackToTop";
 
 export default function Food() {
    return (
